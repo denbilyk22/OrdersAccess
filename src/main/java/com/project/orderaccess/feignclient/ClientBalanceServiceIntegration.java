1a143715773107f567c552cbb30c1f71590c1a9f
Den <denbilyk22021998@gmail.com>
2025-08-15 03:00:14 +0300

    changed url and removed not used method in client balance service integration

--- src/main/java/com/project/orderaccess/feignclient/ClientBalanceServiceIntegration.java
+++ src/main/java/com/project/orderaccess/feignclient/ClientBalanceServiceIntegration.java
@@ -1,18 +1,12 @@
 package com.project.orderaccess.feignclient;
 
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 
-import java.util.UUID;
-
-@FeignClient(name = "client-balance-service", url = "${urls.orders-api}/client-balance")
+@FeignClient(name = "client-balance-service", url = "${urls.orders-api}/client-balances")
 public interface ClientBalanceServiceIntegration {
 
     @PutMapping("/refresh-all")
     void refreshProfitForAllClients();
-
-    @PutMapping("/{clientId}/refresh")
-    void refreshProfitForClient(@PathVariable UUID clientId);
 
 }
